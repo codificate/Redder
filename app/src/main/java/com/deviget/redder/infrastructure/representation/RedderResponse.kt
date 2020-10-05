e7590aa9d300367f39007a202e2f3a2db4935a77
Servando Reyes <andres92898@gmail.com>
2020-10-04 22:30:11 -0300

    RedditResponse class was renamed.

--- app/src/main/java/com/deviget/redder/infrastructure/representation/RedderResponse.kt
+++ app/src/main/java/com/deviget/redder/infrastructure/representation/RedderResponse.kt
@@ -2,7 +2,7 @@ package com.deviget.redder.infrastructure.representation
 
 import com.google.gson.annotations.SerializedName
 
-data class RedditResponse(
+data class RedderResponse(
     @SerializedName("data") val `data`: Data,
     @SerializedName("data") val kind: String
 )
