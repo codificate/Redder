19db2da8b3a9d3d0aa8f7aa8db4b31a96e7d56b1
Servando Reyes <andres92898@gmail.com>
2020-10-04 22:30:26 -0300

    Domain Post class was created.

---
+++ app/src/main/java/com/deviget/redder/domain/Post.kt
@@ -0,0 +1,18 @@
+package com.deviget.redder.domain
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Post(
+    val author: String,
+    val created: Long,
+    val domain: String,
+    val id: String,
+    val num_comments: Int,
+    val readed: Boolean,
+    val subreddit_name_prefixed: String,
+    val thumbnail: String,
+    val title: String,
+    val url: String
+) : Parcelable
