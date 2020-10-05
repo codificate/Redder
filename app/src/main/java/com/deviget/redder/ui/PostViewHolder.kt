cb10685e763493b2b4af97eb30cb6368c2ef4e0b
Servando Reyes <andres92898@gmail.com>
2020-10-05 17:19:45 -0300

    Glide dependencies were included in the project in order to show the post thumbnail image on viewholder class

--- app/src/main/java/com/deviget/redder/ui/PostViewHolder.kt
+++ app/src/main/java/com/deviget/redder/ui/PostViewHolder.kt
@@ -1,8 +1,11 @@
 package com.deviget.redder.ui
 
 import android.view.View
+import android.view.View.VISIBLE
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.deviget.redder.R
 import com.deviget.redder.domain.model.Post
 import com.deviget.redder.utils.TimeHelper
@@ -14,13 +17,21 @@ class PostViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
     private val createdAt = view.findViewById<TextView>(R.id.redder_created_at)
     private val redderTitle = view.findViewById<TextView>(R.id.redder_title)
     private val commentsNumber = view.findViewById<TextView>(R.id.redder_comments_number)
+    private val imageThumbnail = view.findViewById<ImageView>(R.id.redder_image)
 
     fun bind(post: Post) {
         prefix.text = post.subreddit_name_prefixed
-        userName.text = post.author
+        userName.text = "u/"+post.author
         createdAt.text = TimeHelper.timeAgo(post.created)
         redderTitle.text = post.title
         commentsNumber.text = calculateCommentsK(post.num_comments)
+        post.thumbnail?.let { thumbnail ->
+            Glide.with(view.context)
+                .load(thumbnail)
+                .fitCenter()
+                .into(imageThumbnail)
+            imageThumbnail.visibility = VISIBLE
+        }
     }
 
     private fun calculateCommentsK(comments: Int): String {
---
+++ app/src/main/java/com/deviget/redder/utils/glide/RedderGlideModuleV4.kt
@@ -0,0 +1,18 @@
+package com.deviget.redder.utils.glide
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+
+@GlideModule
+class RedderGlideModuleV4 : AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        builder.setDefaultRequestOptions(
+            RequestOptions().format(DecodeFormat.PREFER_RGB_565)
+        )
+    }
+}
